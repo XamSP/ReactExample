eb3467516975eb9e76cfdcb1c7e3d5f836c1744d
XamSP <max0720x@gmail.com>
2018-10-18 23:06:51 -0400

    added forms to it

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import Header from './components/Header';
-import { listItems } from './components/ListDemo';
+//import { listItems } from './components/ListDemo';
 import './App.css';
-import {MoviesList} from './components/ListDemo';
+//import {MoviesList} from './components/ListDemo';
 import DynamicMoviesList from './dynamicListsDemo/DynamicMoviesList';
+import TextareaAndSelectTagDemo from './dynamicListsDemo/TextareaAndSelectTagDemo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header></Header>
-        <ul>{ listItems }</ul>
-        <div>
+        {/*  <ul className="list-style">{ listItems }</ul>
+         <div>
           <MoviesList />
-        </div>
+        </div> */}
         <hr />
         <DynamicMoviesList />
+        <TextareaAndSelectTagDemo />
       </div>
     );
   }
--- src/dynamicListsDemo/DynamicMoviesList.js
+++ src/dynamicListsDemo/DynamicMoviesList.js
@@ -1,49 +1,83 @@
 import React, { Component } from 'react';
 import ImprovedCard from './ImprovedCard'; 
+import AddMovie from './AddMovie';
 
 class DynamicMoviesList extends Component {
   constructor(){
       super();
       this.state = {
-        movies: [
-          { title: "The Godfather", director: "Francis Coppola" },
-          { title: "Star Wars", director: "Rian Johnson" },
-          { title: "The Shawshank Redemption", director: "Frank Darabont" }
-        ]
+          movies: [
+              { title: "The Godfather", director: "Francis Coppola", hasOscars: true, IMDbRating: 9.2 },
+              { title: "Star Wars", director: "Rian Johnson" , hasOscars: true, IMDbRating: 8.7 },
+              { title: "The Shawshank Redemption", director: "Frank Darabont", hasOscars: false, IMDbRating: 9.3 }
+          ],
+          showOscarAwarded: false // <== add
       }
   }
-
-  deleteMovieHandler = (movieIndex) => {
-    const moviesCopy = [...this.state.movies]; 
-    moviesCopy.splice(movieIndex, 1);
+  
+  addMovieHandler = (theMovie) => {
+    const moviesCopy = [...this.state.movies];
+    moviesCopy.push(theMovie);
     this.setState({
-        movies: moviesCopy
+      movies: moviesCopy
     })
+  }
+
+  toggleMovies = () => {
+      this.setState({
+          movies: [
+              { title: "The Godfather", director: "Francis Coppola", hasOscars: true, IMDbRating: 9.2 },
+              { title: "Star Wars", director: "Rian Johnson" , hasOscars: true, IMDbRating: 8.7 },
+              { title: "The Shawshank Redemption", director: "Frank Darabont", hasOscars: false, IMDbRating: 9.3 }
+          ],
+          showOscarAwarded: !this.state.showOscarAwarded
+      })
+  }
+  
+  deleteMovieHandler = (movieIndex) => {
+      const moviesCopy = [...this.filteredMovies];
+      moviesCopy.splice(movieIndex, 1);
+      this.setState({
+          movies: moviesCopy
+      })
   }
 
   addHarryPotter = () => {
     const moviesCopy = [...this.state.movies]; 
-    moviesCopy.push({title: "Harry Potter", director: "Rowen"});
+    moviesCopy.push({title: "Harry Potter", director: "Rowen", hasOscars: false, IMDbRating: '9'});
     this.setState({
         movies: moviesCopy
     })
   }
 
+  filteredMovies;
 
   render() {
-    console.log(this.state.movies);
-    return (
-      <div>
-        {
-          this.state.movies.map((oneMovie, index) => {
-            //return <ImprovedCard key={index} {...oneMovie} />
-            return <ImprovedCard key={index} {...oneMovie} clickToDelete={() => this.deleteMovieHandler(index)} />
-            })
-        }
-        <button onClick={this.addHarryPotter}>Add Harry Potter</button>
-      </div>
-    );
+      console.log(this.state.movies);
+
+      const {showOscarAwarded} = this.state;
+
+      this.filteredMovies = this.state.movies.filter(theMovie => theMovie.hasOscars === showOscarAwarded);
+
+      return (
+          <div>
+              <AddMovie addTheMovie={this.addMovieHandler} />
+              {
+                  this.filteredMovies.map((oneMovie, index) => {
+                      // return <ImprovedCard key={index} {...oneMovie} clickToDelete={this.deleteMovieHandler.bind(this, index)} />
+                      return <ImprovedCard key={index} {...oneMovie} clickToDelete={() => this.deleteMovieHandler(index)} />
+                  })
+              }            
+              <button onClick={() => this.toggleMovies() }>
+                  {showOscarAwarded ? 'Hide Oscar Awarded' : 'Show Oscar Awarded'}
+              </button>
+              <button onClick={this.addHarryPotter}>Add Harry Potter</button>
+
+        </div>
+      );
   }
 }
 
 export default DynamicMoviesList;
+
+
